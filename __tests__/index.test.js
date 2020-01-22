1a8483dae080cf50e6a56a3093ec0d6ce74e00d3
stratis <stratis.vip@gmail.com>
2020-01-22 14:22:58 +0200

    test ready

---
+++ __tests__/index.test.js
@@ -0,0 +1,9 @@
+'use strict'
+import films from '../src/films'
+describe('Test functions in index.js', function(){
+    
+   test('', ()=> {
+       expect(1).toBe(1)
+   })
+
+})
---
+++ src/index.js
@@ -0,0 +1,4 @@
+'use strict'
+import films from './films'
+
+console.log(films)
